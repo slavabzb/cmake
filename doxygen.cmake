2b562a751522dd27e5b01d973bab354bcef71c7a
Vjacheslav Bezborodov <waww@live.ru>
2015-12-15 21:28:11 +0500

    Fix bugs

--- doxygen.cmake
+++ doxygen.cmake
@@ -7,6 +7,7 @@ if(DOXYGEN_FOUND)
 	set(DOCS_DIA_DIR "${DOCS_SRC_DIR}/dia")			# A directory where Dia diagram files (*.dia) is located
 	set(DOCS_LOG_FILE "doxygen.log")				# A doxygen log file
 	set(DOCS_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}") # A directory where docs will be installed
+        set(DOCS_EXAMPLE_DIR "${CMAKE_SOURCE_DIR}/examples")
 	set(DOCS_DIRS									# The directories to be scanned by doxygen
 		"${TARGET_INCLUDE_PATH}/sgl/ \\
 		${DOCS_SRC_DIR}")
@@ -15,7 +16,6 @@ if(DOXYGEN_FOUND)
 
 	add_custom_target(docs ALL
 		${CMAKE_COMMAND} -E make_directory ${DOCS_OUT_DIR}
-		COMMAND rm "${CMAKE_BINARY_DIR}/${DOCS_LOG_FILE}"
 		COMMAND doxygen
 		COMMAND test -f "${CMAKE_BINARY_DIR}/${DOCS_LOG_FILE}" && echo "Error occurred while doxygen compiling documentation. Check ${DOCS_LOG_FILE}")
 
